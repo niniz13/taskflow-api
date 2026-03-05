a6a8cb999cf99577aab4d7b325dd1c04dac04137
Jérémy Gross <jeremspam1307@gmail.com>
2026-03-05 10:51:07 +0100

    feat: architecture modulaire (5 modules) + Docker Compose + ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TeamsModule } from './teams/teams.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+    UsersModule,
+    TeamsModule,
+    ProjectsModule,
+    TasksModule,
+    CommentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
